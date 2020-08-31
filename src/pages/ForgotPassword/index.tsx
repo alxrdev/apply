fd306a8034619aa37d2cf04b023b72f860626d65
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-08-31 11:35:01 -0300

    ForgotPassword page has been refactored

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import SimpleHeader from '../../components/SimpleHeader'
+import SmallContainer from '../../components/SmallContainer'
+import InputGroup from '../../components/InputGroup'
+import Button from '../../components/Button'
 
 import './styles.scss'
 
@@ -9,20 +12,23 @@ const ForgotPassword = () => (
   <div className="forgot-password">
     <SimpleHeader />
 
-    <div className="container-small">
+    <SmallContainer>
       <h1 className='title'>Recover your password</h1>
 
-      <form action="#">
-        <div className="input-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
-        </div>
+      <form>
+        <InputGroup
+          type='email'
+          id='email'
+          name='email'
+          label='Email'
+          value=''
+        />
 
-        <button type="button" className="button button-primary button-block">Send</button>
+        <Button type='primary' isBlock={true} content='Send' />
       </form>
 
       <p className="dont-have-account">Don&apos;t have an account? <Link to="signup">Sign up</Link></p>
-    </div>
+    </SmallContainer>
   </div>
 )
 
