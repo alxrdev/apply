593d3957187bde4106d40ae87fb3a5732eedc36a
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-08-31 12:03:01 -0300

    Created a Header componente

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './styles.scss'
+import logoImage from '../../assets/img/logo.svg'
+
+const Header: React.FC = ({ children }) => (
+  <div className="header">
+    <div className="navbar">
+      <div className="logo">
+        <Link to="/index">
+          <img src={logoImage} alt="Apply" />
+        </Link>
+      </div>
+
+      <div className="right">
+        <ul className="menu">
+          <li><Link to="./login">Apply for a job (Free!)</Link></li>
+          <li className="separator"></li>
+          <li><Link to="./signup">Register a job</Link></li>
+        </ul>
+        <Link to="./login" className="login-button">Log In</Link>
+      </div>
+    </div>
+
+    { children }
+  </div>
+)
+
+export default Header
