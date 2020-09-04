428fb146f835a53b4c1b003b26a33c32b9e66f26
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-09-03 23:48:41 -0300

    Remove unused import

--- src/pages/LogIn/index.tsx
+++ src/pages/LogIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState, ChangeEvent } from 'react'
+import React, { FormEvent, useState } from 'react'
 import { Link } from 'react-router-dom'
 import * as Yup from 'yup'
 
