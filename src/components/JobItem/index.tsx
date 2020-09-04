294c2576016dc8de24b5882946dc9a3b315d77fc
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-09-04 12:04:54 -0300

    Refactored to use functional components

--- src/components/JobItem/index.tsx
+++ src/components/JobItem/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useContext, useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { FiBriefcase, FiDollarSign } from 'react-icons/fi'
 
@@ -11,88 +11,65 @@ interface Props {
   job: Job
 }
 
-interface State {
-  actived: boolean
-}
+const JobItem: React.FC<Props> = ({ job }) => {
+  const { setActiveJob } = useContext(ActiveJobContext)
+  const jobItemRef = useRef<HTMLDivElement>(null)
+  const [active, setActive] = useState(false)
 
-class JobItem extends React.Component<Props, State> {
-  private refContainer: React.RefObject<HTMLDivElement>
+  const { title, employeer, address, jobType, salary } = job
 
-  constructor (props: Props) {
-    super(props)
+  useEffect(() => {
+    document.addEventListener('click', deactiveAllJobs, false)
+    return () => document.removeEventListener('click', deactiveAllJobs, false)
+  }, [active])
 
-    this.state = { actived: false }
-
-    this.refContainer = React.createRef<HTMLDivElement>()
-  }
-
-  handleJobClick = (event: React.MouseEvent) => {
-    this.toggleJob()
-  }
-
-  toggleJob = () => {
-    this.setState({ actived: !this.state.actived }, () => {
-      // change the active job in the context
-      this.context.setActiveJob((this.state.actived) ? this.props.job : null)
-    })
+  const toggleJob = () => {
+    setActiveJob((!active) ? job : null)
+    setActive(!active)
   }
 
   // deactive the job if the user click out
-  deactiveAllJobs = (event: MouseEvent) => {
-    const elements = Array.prototype.map.call(this.refContainer?.current?.children, (element) => element)
+  const deactiveAllJobs = (event: MouseEvent) => {
+    const elements = Array.prototype.map.call(jobItemRef.current?.children, (element) => element)
 
     const isThis = elements.includes(event.target)
 
-    if (!isThis && this.state.actived) {
-      this.setState({ actived: false })
+    if (!isThis && active) {
+      setActive(false)
     }
   }
 
-  componentDidMount () {
-    document.addEventListener('click', this.deactiveAllJobs, true)
-  }
-
-  componentWillUnmount () {
-    document.removeEventListener('click', this.deactiveAllJobs, true)
-  }
-
-  render () {
-    const { title, employeer, address, jobType, salary } = this.props.job
-
-    return (
-      <div
-        className={`job-item ${this.state.actived ? 'active' : ''}`}
-        onClick={this.handleJobClick}
-        ref={this.refContainer}
-      >
-        <div className="avatar">
-          <img src="https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/53685cff8c94982b0b74b40fc78a4dcf" alt={title} />
+  return (
+    <div
+      className={`job-item ${active ? 'active' : ''}`}
+      onClick={toggleJob}
+      ref={jobItemRef}
+    >
+      <div className="avatar">
+        <img src="https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/53685cff8c94982b0b74b40fc78a4dcf" alt={title} />
+      </div>
+      <div className="job-content">
+        <div className="title-employeer">
+          <h5>{ title }</h5>
+          <span><Link to='/'>{ employeer }</Link> <span>14 days ago</span></span>
         </div>
-        <div className="job-content">
-          <div className="title-employeer">
-            <h5>{ title }</h5>
-            <span><Link to='/'>{ employeer }</Link> <span>14 days ago</span></span>
-          </div>
-          <div className="location">
-            <h5>{ address.city }, { address.state }</h5>
-            <span>
-              <FiBriefcase size={15} />
-              <span>{ jobType }</span>
-            </span>
-          </div>
-          <div className="salary">
-            <h5>R$ { salary }</h5>
-            <span>
-              <FiDollarSign size={15} />
-              <span>Per Month</span>
-            </span>
-          </div>
+        <div className="location">
+          <h5>{ address.city }, { address.state }</h5>
+          <span>
+            <FiBriefcase size={15} />
+            <span>{ jobType }</span>
+          </span>
+        </div>
+        <div className="salary">
+          <h5>R$ { salary }</h5>
+          <span>
+            <FiDollarSign size={15} />
+            <span>Per Month</span>
+          </span>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-JobItem.contextType = ActiveJobContext
 
 export default JobItem
