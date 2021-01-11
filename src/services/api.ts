ca6a962bb618792723cad70ca33c7ee848fc2024
Alex Rodrigues <rodriguesalex793@gmail.com>
2021-01-11 14:12:30 -0300

    Api base url has been fixed

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   withCredentials: true,
-  baseURL: 'http://apply-backend.herokuapp.com/api/'
+  baseURL: 'https://apply-backend.herokuapp.com/api/'
 })
 
 export default api
